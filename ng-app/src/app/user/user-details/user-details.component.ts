43eaed18e23311328e6c6d30c8d6b6994658d532
ravi4rails <ravi4rails@gmail.com>
2020-03-17 19:06:18 +0530

    added user details

---
+++ ng-app/src/app/user/user-details/user-details.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { User } from '../user';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent implements OnInit {
+   @Input() user: User;
+  id: number;
+  routeId: any;
+  returnUrl: string;
+  editBtnClicked: boolean = false;
+  errorMessage: any;
+  constructor(
+    private http: Http,
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+  ) { }
+
+  ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/users'
+    this.routeId = this.route.params
+      .subscribe(
+        params => {
+          console.log(+params['id']);
+          this.id = +params['id'];
+        }
+      )
+    let userRequest = this.route.params
+      .flatMap((params: Params) =>
+        this.userService.getUser(+params['id'])
+      );
+    userRequest.subscribe(
+      response => this.user = response.json()
+    );  
+  }
+
+  update(user: User){
+    this.editBtnClicked = true;
+    this.userService.updateUser(this.user)
+      .subscribe(data => {
+        return true
+      }, error => {
+        console.log('Error Editing Post');
+        return Observable.throw(error);
+      })
+  }
+
+  onUpdateClicked() {
+    this.router.navigate([this.returnUrl]);
+    this.editBtnClicked = false;
+  }
+
+  delete(user: User): void {
+    this.userService.deleteUser(this.user.id)
+      .subscribe(data => {
+        this.router.navigate([this.returnUrl]);
+      }, error => this.errorMessage = error);
+  }
+
+}
